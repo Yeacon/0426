8cd5bd5c0c637e1a564d339ea53e3863e30a29a2
Yeacon <horse911127@gmail.com>
2023-05-10 20:43:07 +0800

    0510

---
+++ Web411018531/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411018531.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hw3()
+        {
+            return View();
+        }
+    }
+}
